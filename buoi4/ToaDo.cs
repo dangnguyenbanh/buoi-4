6797618cedeae61301822d543b975d72e3cfc16d
dangnguyenbanh <nguyen09727568@gmail.com>
2023-09-06 13:43:06 +0700

    add ToaDo

---
+++ buoi4/ToaDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi4
+{
+    class ToaDo
+    {
+        private string _diem;
+
+        public string Diem
+        {
+            get { return Diem; }
+            set { _diem = value; }
+        }
+        private float x;
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private float y;
+    }
+}
