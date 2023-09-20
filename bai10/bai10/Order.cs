b061dff892a61d4b7da3db1c65d47ff8cb6e09b2
dangnguyenbanh <142389367+dangnguyenbanh@users.noreply.github.com>
2023-09-20 09:06:30 +0700

    Add files via upload

---
+++ bai10/bai10/Order.cs
@@ -0,0 +1,67 @@
+﻿using bai10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bai10
+{
+    class Order
+    {
+        private int _orderID;
+
+        public int OrderID
+        {
+            get { return _orderID; }
+            set { _orderID = value; }
+        }
+        private DateTime _orderDate;
+
+        public DateTime OrderDate
+        {
+            get { return _orderDate; }
+            set { _orderDate = value; }
+        }
+
+        private List<OrderDetail> _lineItems;
+
+        internal List<OrderDetail> LineItems
+        {
+            get { return _lineItems; }
+            set { _lineItems = value; }
+        }
+        public Order(int orderID, DateTime orderDate, List<OrderDetail> list)
+        {
+            this.OrderID = orderID;
+            this.OrderDate = orderDate;
+            this.LineItems = list;
+        }
+
+        public double CalcTotalCharge()
+        {
+            double ThanhTien = 0;
+            foreach (OrderDetail Item in LineItems)
+            {
+                ThanhTien += Item.CalcTotalPrice();
+            }
+            return ThanhTien;
+        }
+
+        public void ShowOrder()
+        {
+            Console.WriteLine("Ma hoa don: {0}", OrderID);
+            Console.WriteLine("Ngay: {0}", OrderDate);
+            Console.WriteLine("{0,-4}|{1, -8}|{2, -12}|{3,8}|{4,8}|{5,12}", "STT", "Ma SP", "Mo ta", "Gia", "SL", "Thanh tien");
+            int count = 1;
+            foreach (OrderDetail Item in LineItems)
+            {
+                Console.WriteLine("{0,-4}|{1, -8}|{2, -12}|{3,8}|{4,8}|{5,12} VNĐ", count, Item.Product.ProductID, Item.Product.Description, Item.Product.Price, Item.Quatity, Item.CalcTotalPrice());
+                count++;
+            }
+            Console.WriteLine("Tong tien: {0} VNĐ", CalcTotalCharge());
+        }
+
+    }
+}
---
+++ bai10/bai10/Program.cs
@@ -0,0 +1,25 @@
+﻿using bai10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<OrderDetail> lineItems = new List<OrderDetail>();
+            lineItems.Add(new OrderDetail(new Product("Nuoc tuong", "sp4", 8000), 10));
+            lineItems.Add(new OrderDetail(new Product("Gao", "sp1", 18000), 5));
+            lineItems.Add(new OrderDetail(new Product("Duong", "sp3", 10000), 1));
+            lineItems.Add(new OrderDetail(new Product("Gao", "sp1", 18000), 1));
+
+            Order or = new Order(1, new DateTime(2015, 09, 10), lineItems);
+            or.ShowOrder();
+            Console.ReadLine();
+        }
+    }
+}
